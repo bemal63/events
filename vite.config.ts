52c0b4858d00e2a1825f3ce8331fa69a00bf649e
Bemal <bimal163@gmail.com>
2024-01-26 01:05:33 +0400

    Организовал структуру проекта согласно архитектуре FSD, так же добавил альясы в vite.config и tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      app: 'src/app',
+      assets: 'src/assets',
+      entities: 'src/entities',
+      features: 'src/features',
+      pages: 'src/pages',
+      widgets: 'src/widgets'
+    }
+  }
 })
