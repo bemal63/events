6d9d94fab54eabc47ec4cd794b31fce75352085b
Bemal <bimal163@gmail.com>
2025-02-01 14:14:22 +0400

    Установка зависимостей для настройки firebase. (на будущее, ради тестирования)

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+//Отложим пока разворачивания на firebase. 
